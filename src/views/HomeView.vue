6c5c69e863c1ca01f41b9d1b19a07d759921e326
fros-tech <fros@techcollege.dk>
2024-09-23 12:58:11 +0200

    router-link with variable value

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora minima eos a repellat ratione fugit animi quasi pariatur nam, at officia enim odio quae facere, quibusdam, repudiandae commodi voluptate autem architecto delectus velit! Et doloribus necessitatibus aliquam porro ad ipsa eaque quia quam labore eius officia itaque quod laborum ullam odit enim illo dignissimos, nulla nam non mollitia beatae facilis. Illo consequatur maxime doloremque beatae est at totam. Nobis explicabo, odit dicta corrupti nulla reprehenderit placeat quas? Recusandae in, sapiente, amet, at libero accusantium laudantium fuga autem iusto aperiam deserunt reiciendis earum dolorem quis eum saepe. Perspiciatis doloribus libero neque.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora minima eos a repellat ratione fugit animi quasi pariatur nam, at officia enim odio quae facere, quibusdam, repudiandae commodi voluptate autem architecto delectus velit! Et doloribus necessitatibus aliquam porro ad ipsa eaque quia quam labore eius officia itaque quod laborum ullam odit enim illo dignissimos, nulla nam non mollitia beatae facilis. Illo consequatur maxime doloremque beatae est at totam. Nobis explicabo, odit dicta corrupti nulla reprehenderit placeat quas? Recusandae in, sapiente, amet, at libero accusantium laudantium fuga autem iusto aperiam deserunt reiciendis earum dolorem quis eum saepe. Perspiciatis doloribus libero neque.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
